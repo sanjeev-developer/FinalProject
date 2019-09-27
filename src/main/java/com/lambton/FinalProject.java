d011b280f0017a8419ae870dfe7361e8f4670ca0
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-09-26 20:54:02 -0400

    kuldeep commit

--- src/main/java/com/lambton/FinalProject.java
+++ src/main/java/com/lambton/FinalProject.java
@@ -3,6 +3,6 @@ package com.lambton;
 public class FinalProject {
 
     public static void main(String args[]){
-        System.out.println("Hello");
+        System.out.println("yo");
     }
     }
