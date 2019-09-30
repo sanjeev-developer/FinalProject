f9b851a80317169aa8f6a2d8e27d369dd2286625
The Shimmering World <sanjeevgupta9223@gmail.com>
2019-09-30 16:46:40 -0700

    Created the employee (the base class)

---
+++ src/main/java/employee/Employee.java
@@ -0,0 +1,20 @@
+package employee;
+
+public abstract class Employee {
+
+    String Name;
+    Float Age;
+    int totalearning;
+
+    public String calculatebirthyear()
+    {
+
+        return Name;
+    }
+
+    public int calculateearning()
+    {
+
+        return totalearning;
+    }
+}
