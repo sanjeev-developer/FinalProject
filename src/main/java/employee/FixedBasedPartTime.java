05ae319f887b7ff6d1a58258b1d8ef9256443fdf
ritik <ritikjagpal@gmail.com>
2019-09-30 20:33:08 -0400

    commit by ritik

---
+++ src/main/java/employee/FixedBasedPartTime.java
@@ -0,0 +1,9 @@
+package employee;
+
+public class FixedBasedPartTime {
+    double fixedAmount;
+    public void calcEarnings(float rate, float hoursWorked , float fixedAmount)
+    {
+        //return(rate*hoursWORKED) + commission
+    }
+}
