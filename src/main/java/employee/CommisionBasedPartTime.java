6c344a0e81a37bd0fc61e632f3626b2f4d3b4e9a
ritik <ritikjagpal@gmail.com>
2019-09-30 20:31:03 -0400

    commit by ritik

--- src/main/java/employee/CommisionBasedPartTime.java
+++ src/main/java/employee/CommisionBasedPartTime.java
@@ -2,7 +2,7 @@ package employee;
 
 public class CommisionBasedPartTime {
     double commisionPerc;
-    public void calcEarnings(){
-
+    public void calcEarnings(float rate, float hoursWorked , float commission){
+        //return(rate*hoursWORKED) + commission
     }
 }
