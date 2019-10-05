590e231e7746a080e04dbd47ac7938441258191d
kuldeepbamrah <boby.sbamrah@gmail.com>
2019-10-04 20:20:22 -0400

    creating a new class for parsing JSON- [Kuldeep]

--- src/main/java/vehicle/Car.java
+++ src/main/java/vehicle/Car.java
@@ -1,12 +1,14 @@
 package vehicle;
 
 import com.lambton.iPrintable;
+import com.sun.org.apache.bcel.internal.generic.INEG;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class Car extends Vehicle implements iPrintable
 {
     JSONObject jsonObject;
+    long doors;
     String model, yearOfMake;
     Boolean insurance;
 
@@ -16,6 +18,8 @@ public class Car extends Vehicle implements iPrintable
         super.Plate = (String) jsonObject.get("plate");
         model = (String) jsonObject.get("model");
         insurance = (Boolean) jsonObject.get("insurance");
+        yearOfMake=(String) jsonObject.get("year");
+        doors= (long) jsonObject.get("doors");
     }
 
 
@@ -26,9 +30,13 @@ public class Car extends Vehicle implements iPrintable
         System.out.println("Manufacturer: " + make );
         System.out.println("Licence Plate : " +Plate);
         System.out.println("Model :" +model);
+        System.out.println("Year Of Make : " +yearOfMake);
         if(insurance=true)
             System.out.println("Insurance : Yes");
         else
             System.out.println("Insurance : No");
+        System.out.println("Number of Doors: " +doors);
+
+        System.out.println("*********************************************************");
     }
 }
